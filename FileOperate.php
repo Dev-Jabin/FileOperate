168d3f92313748fa2d91ea97e8d6bb5d9bfafe7c
Jabin <jpyan727@2906.com>
2019-06-06 13:52:23 +0800

    rename file

--- FileOperate.php
+++ FileOperate.php
@@ -20,8 +20,23 @@ class File
 			}
 		}
 	}
+
+	//写入文件
+	public function writeFile($file)
+	{
+		$handle = fopen($file, 'r');
+		$content = fread($handle, filesize($file));
+		$content = "hello File\n".$content;
+		fclose($handle);
+		// write
+		$handle = fopen($file, 'w');
+		fwrite($handle, $content);
+		fclose($handle);
+	}
 }
 
 $fileObj = new File();
 $dir = 'test';
 $fileObj -> loopDir($dir);
+$file = 'test/test1/demo1.txt';
+$fileObj -> writeFile($file);
